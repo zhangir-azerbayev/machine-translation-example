b2040cf726f498d8520d98b38267f62cb80da972
zhangir-azerbayev <zazerbayev@gmail.com>
2022-07-11 17:14:48 -0400

    stuff

--- data/tokenizer/test_tokenizer.py
+++
@@ -1,9 +0,0 @@
-from transformers import PreTrainedTokenizerFast as Tokenizer
-
-tokenizer = Tokenizer(tokenizer_file="en_tokenizer.json")
-
-text = "the quick brown fox jumps over the lazy dog?"
-
-print(tokenizer.decode(tokenizer.encode(text)))
-
-
